b9826b478aa5f62b265d5755701a521eaaba20a3
mihika-agrawal <71880300+mihika-agrawal@users.noreply.github.com>
2020-11-02 16:05:56 +0530

    Update slingshot.js

--- slingshot.js
+++ slingshot.js
@@ -4,7 +4,7 @@ class SlingShot{
             bodyA: bodyA,
             pointB: pointB,
             stiffness: 0.04,
-            length: 10
+            length: 1
         }
         this.pointB = pointB
         this.sling = Constraint.create(options);
